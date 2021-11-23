805769eba0389c6825e584ea0c913af511bb3460
Tomohiro Sasaki <takatobi0326@icloud.com>
2021-11-24 07:49:10 +0900

    Initial Commit

---
+++ PlayMusic/PlayMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayMusicApp.swift
+//  PlayMusic
+//
+//  Created by user on 2021/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
