0ac9c3c1a3fa2163aeb29f6ec7633d6dbcb3063e
Tomohiro Sasaki <takatobi0326@icloud.com>
2022-01-16 19:26:50 +0900

    Game用音楽アプリケーション ・実装修正前

--- PlayMusic/ContentView.swift
+++ PlayMusic/ContentView.swift
@@ -8,14 +8,108 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let soundPlayer = SoundPlayer()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                Group{
+                    //1
+                    Button {
+                        soundPlayer.playMusic1()
+                    } label: {
+                        Text("a")
+                    }
+                    //2
+                    Button {
+                        soundPlayer.playMusic2()
+                    } label: {
+                        Text("b")
+                    }
+                    //3
+                    Button {
+                        soundPlayer.playMusic3()
+                    } label: {
+                        Text("c")
+                    }
+                    //4
+                    Button {
+                        soundPlayer.playMusic4()
+                    } label: {
+                        Text("d")
+                    }
+                    //5
+                    Button {
+                        soundPlayer.playMusic5()
+                    } label: {
+                        Text("e")
+                    }
+                    //6
+                    Button {
+                        soundPlayer.playMusic6()
+                    } label: {
+                        Text("f")
+                    }
+                    //7
+                    Button {
+                        soundPlayer.playMusic7()
+                    } label: {
+                        Text("g")
+                    }
+                    //8
+                    Button {
+                        soundPlayer.playMusic8()
+                    } label: {
+                        Text("h")
+                    }
+                    //9
+                    Button {
+                        soundPlayer.playMusic9()
+                    } label: {
+                        Text("i")
+                    }
+                    //10
+                    Button {
+                        soundPlayer.playMusic10()
+                    } label: {
+                        Text("j")
+                    }
+//                    //11
+//                    Button {
+//                        soundPlayer.playMusic11()
+//                    } label: {
+//                        Text("k")
+//                    }
+//                    //12
+//                    Button {
+//                        soundPlayer.playMusic12()
+//                    } label: {
+//                        Text("l")
+//                    }
+//                    //13
+//                    Button {
+//                        soundPlayer.playMusic13()
+//                    } label: {
+//                        Text("m")
+//                    }
+//                    //14
+//                    Button {
+//                        soundPlayer.playMusic14()
+//                    } label: {
+//                        Text("n")
+//                    }
+                }
+                
+            }
+            .navigationBarTitle(Text("My Music"))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro")
     }
 }
--- PlayMusic/SoundPlayer.swift
+++ PlayMusic/SoundPlayer.swift
@@ -10,20 +10,60 @@ import AVFoundation
 
 class SoundPlayer: NSObject {
     
-    //music.linkのようにクラスのプロパティやクラスのメソッドを参照するときは関数の中で定義しなければならない
-    var music : Music
+    var musicContent : [String] = [
+        "Adventures",
+        "Blank",
+        "Candyland",
+        "Earth",
+        "Fade",
+        "Feel Good",
+        "Hope",
+        "Jim Yosef",
+        "Light It Up",
+        "My Heart",
+        "Nekozilla",
+        "On & On",
+        "Shine",
+        "Stmbolism"
+    ]   
     
-    init(music:Music){
-        self.music = music
-    }
+    //    let musicData1 = NSDataAsset(name: "Adventures")!.data
+    //
+    //    let musicData2 = NSDataAsset(name: "Blank")!.data
+    //
+    //    let musicData3 = NSDataAsset(name: "Candyland")!.data
+    //
+    //    let musicData4 = NSDataAsset(name: "Earth")!.data
+    //
+    //    let musicData5 = NSDataAsset(name: "Fade")!.data
+    //
+    //    let musicData6 = NSDataAsset(name: "Feel Good")!.data
+    //
+    //    let musicData7 = NSDataAsset(name: "Hope")!.data
+    //
+    //    let musicData8 = NSDataAsset(name: "Jim Yosef")!.data
+    //
+    //    let musicData9 = NSDataAsset(name: "Light It Up")!.data
+    //
+    //    let musicData10 = NSDataAsset(name: "My Heart")!.data
+    //
+    //    let musicData11 = NSDataAsset(name: "Nekozilla")!.data
+    //
+    //    let musicData12 = NSDataAsset(name: "On & On")!.data
+    //
+    //    let musicData13 = NSDataAsset(name: "Shine")!.data
+    //
+    //    let musicData14 = NSDataAsset(name: "Symbolism")!.data
+    
+    
     //Declare Variable
     var musicPlayer : AVAudioPlayer!
     
-    func playMusic() {
+    func playMusic1() {
         
         do{
-            //Declare SoundData
-            let musicData = NSDataAsset(name: music.link)!.data
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[0])!.data
             
             //import AudioData
             musicPlayer = try AVAudioPlayer(data: musicData)
@@ -36,19 +76,270 @@ class SoundPlayer: NSObject {
         
     }
     
-    func pauseMusic() {
+    func playMusic2() {
+        
         do{
-            //Declare SoundData
-            let musicData = NSDataAsset(name: music.link)!.data
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[1])!.data
             
             //import AudioData
             musicPlayer = try AVAudioPlayer(data: musicData)
             
-            //stop Music
-            musicPlayer.stop()
+            //Play Music
+            musicPlayer.play()
         }catch{
-            print("You hava a Error pause AudioPlayer!!")
+            print("You have a Error import AudioData!!")
         }
     }
     
+    func playMusic3() {
+        
+        do{
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[2])!.data
+            
+            //import AudioData
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            
+            //Play Music
+            musicPlayer.play()
+        }catch{
+            print("You have a Error import AudioData!!")
+        }
+    }
+    
+    func playMusic4() {
+        
+        do{
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[3])!.data
+            
+            //import AudioData
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            
+            //Play Music
+            musicPlayer.play()
+        }catch{
+            print("You have a Error import AudioData!!")
+        }
+    }
+    
+    func playMusic5() {
+        
+        do{
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[4])!.data
+            
+            //import AudioData
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            
+            //Play Music
+            musicPlayer.play()
+        }catch{
+            print("You have a Error import AudioData!!")
+        }
+    }
+    
+    func playMusic6() {
+        
+        do{
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[5])!.data
+            
+            //import AudioData
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            
+            //Play Music
+            musicPlayer.play()
+        }catch{
+            print("You have a Error import AudioData!!")
+        }
+    }
+    
+    func playMusic7() {
+        
+        do{
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[6])!.data
+            
+            //import AudioData
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            
+            //Play Music
+            musicPlayer.play()
+        }catch{
+            print("You have a Error import AudioData!!")
+        }
+    }
+    
+    func playMusic8() {
+        
+        do{
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[7])!.data
+            
+            //import AudioData
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            
+            //Play Music
+            musicPlayer.play()
+        }catch{
+            print("You have a Error import AudioData!!")
+        }
+    }
+    
+    func playMusic9() {
+        
+        do{
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[8])!.data
+            
+            //import AudioData
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            
+            //Play Music
+            musicPlayer.play()
+        }catch{
+            print("You have a Error import AudioData!!")
+        }
+    }
+    
+    func playMusic10() {
+        
+        do{
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[9])!.data
+            
+            //import AudioData
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            
+            //Play Music
+            musicPlayer.play()
+        }catch{
+            print("You have a Error import AudioData!!")
+        }
+    }
+    
+    func playMusic11() {
+        
+        do{
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[10])!.data
+            
+            //import AudioData
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            
+            //Play Music
+            musicPlayer.play()
+        }catch{
+            print("You have a Error import AudioData!!")
+        }
+    }
+    
+    func playMusic12() {
+        
+        do{
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[11])!.data
+            
+            //import AudioData
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            
+            //Play Music
+            musicPlayer.play()
+        }catch{
+            print("You have a Error import AudioData!!")
+        }
+    }
+    
+    func playMusic13() {
+        
+        do{
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[12])!.data
+            
+            //import AudioData
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            
+            //Play Music
+            musicPlayer.play()
+        }catch{
+            print("You have a Error import AudioData!!")
+        }
+    }
+    
+    func playMusic14() {
+        
+        do{
+            
+            //Declare MusicContects
+            let musicData = NSDataAsset(name: musicContent[13])!.data
+            
+            //import AudioData
+            musicPlayer = try AVAudioPlayer(data: musicData)
+            
+            //Play Music
+            musicPlayer.play()
+        }catch{
+            print("You have a Error import AudioData!!")
+        }
+    }
+    
+    //    func playMusic15() {
+    //
+    //        do{
+    //
+    //            //Declare MusicContects
+    //            let musicData = NSDataAsset(name: musicContent[14])!.data
+    //
+    //            //import AudioData
+    //            musicPlayer = try AVAudioPlayer(data: musicData)
+    //
+    //            //Play Music
+    //            musicPlayer.play()
+    //        }catch{
+    //            print("You have a Error import AudioData!!")
+    //        }
+    //    }
+    //
+    //    func playMusic16() {
+    //
+    //        do{
+    //
+    //            //Declare MusicContects
+    //            let musicData = NSDataAsset(name: musicContent[15])!.data
+    //
+    //            //import AudioData
+    //            musicPlayer = try AVAudioPlayer(data: musicData)
+    //
+    //            //Play Music
+    //            musicPlayer.play()
+    //        }catch{
+    //            print("You have a Error import AudioData!!")
+    //        }
+    //    }
+    
+    
+    
+    func pauseMusic() {
+        //pause Music
+        musicPlayer.pause()
+    }
+    
+    func stopMusic(){
+        //stop Music
+        musicPlayer.stop()
+    }
 }
